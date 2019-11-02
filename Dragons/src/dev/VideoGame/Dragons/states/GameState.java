b2bd0d4303581b9765ac8e382f565ff8dd6d8bb6
Geidy <geidydm@gmail.com>
2019-11-01 23:20:39 -0400

    render player and extended state

--- Dragons/src/dev/VideoGame/Dragons/states/GameState.java
+++ Dragons/src/dev/VideoGame/Dragons/states/GameState.java
@@ -2,22 +2,29 @@ package dev.VideoGame.Dragons.states;
 
 import java.awt.Graphics;
 
-import dev.VideoGame.Dragons.gfx.Assets;
+import dev.VideoGame.Dragons.entities.creatures.Player;
+
+//import dev.VideoGame.Dragons.gfx.Assets;
 
 public class GameState extends State{
 	
+	private Player player;
+		
 	public GameState() {
+		player = new Player(100, 100);  //position
 		
 	}
 
 	@Override
 	public void tick() {
+		player.tick();
 				
 	}
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.dirt, 0, 0, null);
+		//g.drawImage(Assets.dirt, 0, 0, null);
+		player.render(g);
 				
 	}
 	
