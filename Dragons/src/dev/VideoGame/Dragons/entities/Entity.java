475eec329042580fbaaeeadee75c4c2849d33ef4
Geidy <geidydm@gmail.com>
2019-11-03 09:04:21 -0500

    Created wisth and height generated getters ans setters

--- Dragons/src/dev/VideoGame/Dragons/entities/Entity.java
+++ Dragons/src/dev/VideoGame/Dragons/entities/Entity.java
@@ -5,14 +5,51 @@ import java.awt.Graphics;
 public abstract class Entity {
 	
 	protected float x, y;   //Achieve smooth movement
-	
+	protected int width, height; //help draw images in diff places on scree
+		
 	
 	public Entity(float x, float y) {
 		this.x = x;
 		this.y = y;
+		this.width = width;
+		this.height = height;
 				
 	}
 	
+	//Getters and setters
+	
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public abstract void tick();
 	
 	public abstract void render(Graphics g);
