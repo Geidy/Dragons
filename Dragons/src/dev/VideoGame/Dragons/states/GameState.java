6e83fbf3aac30cd92b97382f03cf38497bf8b0a0
Geidy <geidydm@gmail.com>
2019-11-06 23:04:08 -0500

    passing and loading world test 1

--- Dragons/src/dev/VideoGame/Dragons/states/GameState.java
+++ Dragons/src/dev/VideoGame/Dragons/states/GameState.java
@@ -5,21 +5,24 @@ import java.awt.Graphics;
 import dev.VideoGame.Dragons.Game;
 import dev.VideoGame.Dragons.entities.creatures.Player;
 import dev.VideoGame.Dragons.tiles.Tile;
+import dev.VideoGame.Dragons.world.World;
 
 //import dev.VideoGame.Dragons.gfx.Assets;
 
 public class GameState extends State{
 	
 	private Player player;
+	private World world;
 		
 	public GameState(Game game) {
 		super(game);
 		player = new Player(game, 100, 100);  //position
-		
+		world = new World("");
 	}
 
 	@Override
 	public void tick() {
+		world.tick();
 		player.tick();
 				
 	}
@@ -27,6 +30,7 @@ public class GameState extends State{
 	@Override
 	public void render(Graphics g) {
 		//g.drawImage(Assets.dirt, 0, 0, null);
+		world.render(g);
 		player.render(g);
 		Tile.tiles[1].render(g, 0, 0);
 				
