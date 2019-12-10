358229d0450654697612e2539f4fe22fee67fc50
Geidy <geidydm@gmail.com>
2019-12-10 13:35:07 -0500

    Created a new flower asset

---
+++ Dragons/src/dev/VideoGame/Dragons/tiles/FlowerTile.java
@@ -0,0 +1,16 @@
+package dev.VideoGame.Dragons.tiles;
+
+import java.awt.image.BufferedImage;
+
+import dev.VideoGame.Dragons.gfx.Assets;
+
+public class FlowerTile extends Tile {
+	
+	public FlowerTile(int id) {
+		
+		super(Assets.flower, id);
+		
+	}
+	
+
+}
